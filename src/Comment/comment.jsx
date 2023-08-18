8366207feae186eba8b40fda4019fb577ad634de
DebanjanChatterjeeMERNSTACK <debanjanthechatterjee@gmail.com>
2023-08-18 09:41:08 -0700

    cHANGES

--- src/Comment/comment.jsx
+++ src/Comment/comment.jsx
@@ -5,29 +5,31 @@ import { MdDelete } from 'react-icons/md';
 import logo from "../logo/emoji.jpeg"
 
 const Comment = () => {
-    const [comment,setcomment]=useState([])
+    const [comment, setcomment] = useState([])
 
-    useEffect(()=>{
-     fetch("http://localhost:9000/commentget")
-     .then(res=>res.json())
-     .then(json=>setcomment(json))
-    },[])
-
-  const handleclick=(id)=>{
-
-    fetch("http://localhost:9000/commentdelete", {
-        method: "post",
-        headers: { "Content-Type": "application/json", },
-        body: JSON.stringify({
-            _id: id
-        })
+    useEffect(() => {
+        fetch("http://localhost:9000/commentget")
+            .then(res => res.json())
+            .then(json => {
+                setcomment(json)
+            })
     })
-        .then(err => err.json())
-        .then(json => alert(json.mess))
-     
+
+    const handleclick = (id) => {
+
+        fetch("http://localhost:9000/commentdelete", {
+            method: "post",
+            headers: { "Content-Type": "application/json", },
+            body: JSON.stringify({
+                _id: id
+            })
+        })
+            .then(err => err.json())
+            .then(json => alert(json.mess))
 
 
-  }
+
+    }
 
 
 
@@ -40,19 +42,22 @@ const Comment = () => {
                     <Header />
                 </div>
                 <div className="gridnav">
-                  { comment.length===0 ? 
-                  <div className="emoji">
-                    <img src={logo} className="emojiimg"/>
-                  </div>
-                  :
-                  comment && comment.map((e,i)=>{
-                     <div className="comment" key={i}>
-                        <p>{e.comment}</p>
-                        <MdDelete className="delete"  onClick={()=>handleclick(e._id)}/>
-                     </div>
+                    {comment.length === 0 ?
+                        <div className="emoji">
+                            <img src={logo} className="emojiimg" />
+                        </div>
+                        :
+                        comment && comment.map((e, i) => {
+                            return (
+                                <div className="comment" key={i}>
+                                    <p>{e.comment}</p>
+                                    <MdDelete className="delete" onClick={() => handleclick(e._id)} />
+                                </div>
 
-                  })
-                  }
+                            )
+
+                        })
+                    }
                 </div>
             </div>
 
